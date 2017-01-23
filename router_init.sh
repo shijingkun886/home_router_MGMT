9f2c104dbcd0f532ec7f3365d0671976f5d728e0
shijingkun886 <shijingkun886@126.com>
2017-01-23 15:04:37 +0800

    update ddns

--- router_init.sh
+++ router_init.sh
@@ -12,6 +12,7 @@ rm -rf /tmp/home/root && ln -s /tmp/mnt/sda1/admin /tmp/home/root
 cru a ScheduleSyncfile "0 4 * * * /tmp/mnt/sda1/MGMT/synctool/autosync.sh >> /tmp/mnt/sda1/MGMT/synctool/log/dvrsync.log 2>&1"
 cru a PowerOnGen8 "0 19 * * 5 /tmp/mnt/sda1/MGMT/synctool/bootServer.sh >> /tmp/mnt/sda1/MGMT/synctool/log/dvrsync.log 2>&1"
 cru a PowerOffGen8 "0 1 * * 1 /tmp/mnt/sda1/MGMT/synctool/offServer.sh >> /tmp/mnt/sda1/MGMT/synctool/log/dvrsync.log 2>&1"
+cru a UpdateAliyunDDNS "*/10 * * * * /jffs/scripts/ddns-start >> /tmp/mnt/sda1/MGMT/synctool/log/dvrsync.log 2>&1"
 
 #set alias
 [ `which vim`X != X ] && alias vi=vim
@@ -24,11 +25,13 @@ accessKeySecret=`grep "accessKeySecret" $DDNS_CONFIG_FILE|sed 's/.*=\ //g'`
 recordID='89863902'
 RR='ssh'
 IP=`ip addr show ppp0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1`
+CURRENT_RESOLVED_IP=`nslookup ssh.shikun.win 223.5.5.5|tail -n 2|grep Address|awk -F": " '{print $2}'`
 
-if [ X"$IP" == X"" ]
+if [ X"$IP" == X"$CURRENT_RESOLVED_IP" ]
 then
-/sbin/ddns_custom_updated 0
-exit -1
+	echo "ssh.shikun.win is up-to-date, not updated it."
+	/sbin/ddns_custom_updated 0
+	exit
 fi
 
 echo "$IP $accessKeyID $accessKeySecret $recordID $RR"
@@ -39,4 +42,4 @@ echo "$IP $accessKeyID $accessKeySecret $recordID $RR"
 
 
 #Setting Thunder
-cd /tmp/mnt/sda1/MGMT/Thunder && ./portal
+#cd /tmp/mnt/sda1/MGMT/Thunder && ./portal
