e264d49159dd63f9da1f9904bfde353719fae7ca
shijingkun886 <shijingkun886@126.com>
2017-10-25 17:52:51 +0800

    sk prepare the status monitor script

---
+++ scripts/gen8_status.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+gen8_address="10.0.0.2"
+
+succ_count=`ping -c5 -W1 $check_address|grep received|awk -F' ' '{print $4}'`
+
+last_power_status=''
+
+if [ $succ_count -lt 5 ]; then
+	echo "gen8 is power off"
+	power_status='off'
+fi
+
+#if status changed, send notify to owner
+
